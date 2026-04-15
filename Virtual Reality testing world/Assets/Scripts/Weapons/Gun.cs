5b5ef657112b235bd02bfaa17bb1ce3a62ebc54a
Zander Ragsdale <128868694+CyberCat45@users.noreply.github.com>
2026-04-15 15:25:51 -0500

    Bullet trails

--- Virtual Reality testing world/Assets/Scripts/Weapons/Gun.cs
+++ Virtual Reality testing world/Assets/Scripts/Weapons/Gun.cs
@@ -1,4 +1,5 @@
 
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -6,9 +7,10 @@ using UnityEngine;
 public class Gun : Weapon
 {
     [SerializeField] GameObject bulletHolePrefab;
-
+    [SerializeField] TrailRenderer bulletTrail;
     [SerializeField] protected float range = 50;
     [SerializeField] protected Transform fireOrigin;
+    [SerializeField] protected float bulletTravelTime = 0.1f;
     public AudioClip fireSound;
     protected bool firing = false;
     protected float bulletSpreadAmount = 1;
@@ -20,6 +22,8 @@ public class Gun : Weapon
         Physics.Raycast(fireOrigin.position, fireDirection, out hit, range, damageMask);
         if (hit.collider != null)
         {
+            TrailRenderer trail = Instantiate(bulletTrail, fireOrigin.position, Quaternion.identity);
+            StartCoroutine(BulletTrail(trail, hit));
             if (hit.collider.GetComponent<IDamageable>() != null)
             {
                 IDamageable enemy = hit.collider.gameObject.GetComponent<IDamageable>();
@@ -50,5 +54,23 @@ public class Gun : Weapon
     {
         base.ReleaseWeapon();
         firing = false;
+    }
+
+    public IEnumerator BulletTrail(TrailRenderer trail, RaycastHit hit)
+    {
+        float time = 0;
+        Vector3 startPosition = trail.gameObject.transform.position;
+
+        while (time < 1)
+        {
+            trail.transform.position = Vector3.Lerp(startPosition, hit.point, time);
+            time += Time.deltaTime / bulletTravelTime;
+
+            yield return null;
+        }
+
+        trail.transform.position = hit.point;
+
+        Destroy(trail, trail.time);
     }
 }
